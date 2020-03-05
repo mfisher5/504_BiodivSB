18beb8704e833c459a6ce5db2b8b44dd0e2a9e60
mfisher5 <mfisher5@uw.edu>
2020-03-05 13:14:08 -0800

    update final df with new by-site calcs, kelp abundance

--- scripts/combine_dataframes.R
+++ scripts/combine_dataframes.R
@@ -13,11 +13,15 @@ library(here)
 # For question 2, I need the kelp, oceanographic, and urchin abundance data.
 
 urchins <- read.csv(here::here("data","Annual_Invert_RichnessAbundDiversity_2001-2018.csv")) %>%
-  dplyr::select(year,month,site,transect, franciscanus.abundance,purpuratus.abundance,urchin.abundance) %>%
-  distinct()
+  dplyr::select(year,month,site,transect,franciscanus.abundance,purpuratus.abundance,urchin.abundance) %>%
+  filter(!is.na(urchin.abundance)) %>%
+  pivot_longer(cols=c(franciscanus.abundance,purpuratus.abundance,urchin.abundance)) %>%
+  group_by(year,month,site,name) %>%
+  summarise(urchin.mean=mean(value,na.rm=TRUE)) %>%
+  pivot_wider(names_from=name,values_from=urchin.mean)
+
 waterchem <- read.csv(here::here("data","Annual_CTD_WaterChemistry_MeanMax_2000-2018.csv"))
-kelp <- read.csv(here::here("data","Annual_Kelp_MeanSize_MeanAbund_2001-2018.csv")) %>%
-  dplyr::select(-scientific_name, -group)
+kelp <- read.csv(here::here("data","Annual_Kelp_MeanSize_MeanAbund_2001-2018.csv"))
 
 temp <- read.csv(here::here("data","Seasonal_Mean_Bottom_Temp_2002-2019.csv")) %>%
   dplyr::select(year,site,season,mean.temp,var.temp) %>%
@@ -25,7 +29,7 @@ temp <- read.csv(here::here("data","Seasonal_Mean_Bottom_Temp_2002-2019.csv")) %
   pivot_longer(cols=c(mean_temp,var_temp)) %>%
   pivot_wider(names_from=c(season,name), values_from=value)
 
-dat2_urchins <- left_join(kelp,urchins,by=c("year","month","site","transect"))
+dat2_urchins <- left_join(kelp,urchins,by=c("year","month","site"))
 dat2 <- left_join(dat2_urchins,waterchem,by=c("site"="station","year"))
 dat2 <- left_join(dat2, temp,by=c("year","site"))
 
